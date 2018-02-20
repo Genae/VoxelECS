eccfa7dd0c743f69adfe30cb09fcafb0cbbd63ab
Genae <stefan@2stark4you.de>
2018-02-20 17:26:04 +0100

    Visualize Unit-Testing problem

---
+++ Assets/Testing/ChunkTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Assets.Testing
+{
+    public class ChunkTest : MonoBehaviour {
+
+        [Test]
+        public void TestChunkData()
+        {
+            //var cd = new ChunkData(); TODO ACCESS Chunk data from here
+        }
+    }
+}
